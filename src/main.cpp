cda7ef493bf6e5fe748dc673a9434a341f23941f
Edwin Eefting <edwin@datux.nl>
2022-04-16 21:51:59 +0200

    conn

--- src/main.cpp
+++ src/main.cpp
@@ -2,35 +2,34 @@
 
 #include <Arduino.h>
 #include <WiFi.h>
+#include <WiFiUdp.h>
 
-#define FASTLED_ESP32_I2S true
+// #define FASTLED_ESP32_I2S true
 #include <FastLED.h>
-
 
 #define NUM_LEDS 300
 #define STRIPS 2
 CRGB leds[STRIPS][NUM_LEDS];
 
-void wificheck()
+void
+wificheck()
 {
+  WiFi.begin(ssid, pass);
   while (WiFi.status() != WL_CONNECTED) {
 
     FastLED.clear();
     FastLED.show();
     delay(500);
-    leds[0][0]=CRGB::Red;
+    leds[0][0] = CRGB::Red;
     FastLED.show();
     delay(500);
 
-
     Serial.print("Attempting to connect to WPA SSID: ");
     Serial.println(ssid);
-    WiFi.begin(ssid, pass);
-
-
+    // WiFi.begin(ssid, pass);
   }
+  Serial.println(WiFi.localIP());
 }
-
 
 void
 setup()
@@ -39,29 +38,11 @@ setup()
   Serial.println("Booted\n");
   FastLED.addLeds<NEOPIXEL, 12>(leds[0], NUM_LEDS);
   FastLED.addLeds<NEOPIXEL, 13>(leds[1], NUM_LEDS);
-  // FastLED.clear();
+  FastLED.clear();
   FastLED.setBrightness(255);
   wificheck();
 }
 
-
-
-void loop()
-{
-  for (int i = 1; i < NUM_LEDS; i++) {
-    leds[0][i-1]=CRGB::Black;
-    leds[0][i] = CRGB::Red;
-    leds[1][i-1]=CRGB::Black;
-    leds[1][i] = CRGB::Green;
-    // int start=millis();
-    // FastLED.show();
-    // int end=millis();
-    // Serial.printf("tijd %i\n" ,end-start);
-    // int start=millis();
-    FastLED.show();
-    // int end=millis();
-    // Serial.println(end-start);
-    delayMicroseconds(1000000);
-    
-  }
-}
+void
+loop()
+{}
