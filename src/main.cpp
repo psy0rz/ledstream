875679d7ce0f0c40eb0d98c6b3d8660ed331f3a3
Edwin Eefting <edwin@datux.nl>
2022-04-18 23:33:46 +0200

    fancyness

--- src/main.cpp
+++ src/main.cpp
@@ -8,32 +8,54 @@
 #include <FastLED.h>
 #include <udpbuffer.hpp>
 #include <multicastsync.hpp>
+#include "utils.h"
 
 CRGB leds[CHANNELS][NUM_LEDS];
 
 UdpBuffer udpBuffer = UdpBuffer();
 MulticastSync multicastSync=MulticastSync();
 
+
+//dutycycle function to make stuff blink without using memory 
+//returns true during 'on' ms, with total periode time 'total' ms.
+bool duty_cycle(unsigned long on, unsigned long total, unsigned long starttime=0)
+{
+  if (!starttime)
+    return((multicastSync.remoteMillis()%total)<on);
+  else
+    return(((multicastSync.remoteMillis()-starttime)%total)<on);
+}
+
+//notify user via led and clearing display 
+//also note that blinking should be in sync between different display via multicastsync
+void notify(CRGB rgb, int on, int total)
+{
+    static bool lastState=false;
+
+    if (duty_cycle(on, total)!=lastState)
+    {
+      lastState=!lastState;
+
+      FastLED.clear();
+      if (lastState)
+        leds[0][0]=rgb;
+      FastLED.show();
+    }
+}
+
+
 void
 wificheck()
 {
   WiFi.begin(ssid, pass);
+  Serial.printf("Attempting to connect to WPA SSID: %s\n", ssid);
   while (WiFi.status() != WL_CONNECTED) {
+    notify(CRGB::Red,125,250);
 
-    FastLED.clear();
-    FastLED.show();
-    delay(500);
-    leds[0][0] = CRGB::Red;
-    FastLED.show();
-    delay(500);
-
-    Serial.print("Attempting to connect to WPA SSID: ");
-    Serial.println(ssid);
   }
-  leds[0][0] = CRGB::Green;
-  FastLED.show();
   Serial.println(WiFi.localIP());
 }
+
 
 void
 setup()
@@ -46,6 +68,7 @@ setup()
   FastLED.addLeds<NEOPIXEL, 12>(leds[0], NUM_LEDS);
   FastLED.addLeds<NEOPIXEL, 13>(leds[1], NUM_LEDS);
   FastLED.clear();
+  FastLED.show();
   FastLED.setBrightness(255);
   wificheck();
 }
@@ -58,7 +81,6 @@ loop()
 
   udpBuffer.begin(65000);
   udpBuffer.reset();
-  // unsigned long lastTime = multicastSync.remoteMicros();
   unsigned long showTime=0;
   uint32_t lastTime=0;
 
@@ -66,19 +88,12 @@ loop()
   bool ready = false;
 
 
-  // float pidDelay=16300;
-  // float nextDelay=pidDelay;
-
-
-  // nextTime=multicastSync.remoteMicros()+interval*(BUFFER_FRAMES/2);
-
-  // byte waitFrames=0;
 
   while (1) {
 
     multicastSync.recv();
 
-    // // recv packets and calc framerate
+    // recv packets and calc framerate
     udpBuffer.recvNext();
 
 
@@ -88,8 +103,6 @@ loop()
         FastLED.show();
           // Serial.printf("avail=%d, showtime=%d \n", udpBuffer.available(),showTime);
 
-        // nextTime=nextTime+(interval*waitFrames);
-        // Serial.println(waitFrames);
         ready = false;
 
       }
@@ -121,6 +134,11 @@ loop()
           memcpy(leds[packet->channel], packet->pixels, sizeof(packet->pixels));
           packet=NULL;
         }
+      }
+      else
+      {
+        notify(CRGB::Green,1000,2000);
+
       }
     }
   }
---
+++ src/utils.h
@@ -0,0 +1,11 @@
+#ifndef _utils_h
+#define _utils_h
+
+void(* reboot) (void) = 0;
+
+
+
+
+
+
+#endif
